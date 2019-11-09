a87f8b4c33c8211c09d144fc87b185a62382443c
Bonfim Justino <bonfim1999@gmail.com>
2019-11-09 16:45:03 -0300

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(".dropdown-trigger").dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false, // Displays dropdown below the button
+        alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    });
+    $('.sidenav').sidenav();
+
+    $('.carousel.carousel-slider').carousel({
+
+        indicators: true,
+    });
+    setInterval(function() {
+        $('.carousel').carousel('next');
+    }, 10000); // every 10 seconds
+});
