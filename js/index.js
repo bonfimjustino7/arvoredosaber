a7cb928a6bc4c98714f8ee219e912f608344684a
Bonfim Justino <bonfim1999@gmail.com>
2019-11-11 12:07:10 -0300

    segundo commit

--- js/index.js
+++ js/index.js
@@ -17,4 +17,18 @@ $(document).ready(function() {
     setInterval(function() {
         $('.carousel').carousel('next');
     }, 10000); // every 10 seconds
+
+    $(".scroll").click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 600);
+    });
+    $(".top").click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 600);
+    });
+    $('.fixed-action-btn').floatingActionButton();
+
+
 });
